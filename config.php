219d58313beb929f0c1e0eeb03b2816289c8d833
Ayron112 <146877747+Ayron112@users.noreply.github.com>
2025-06-06 05:00:34 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // config.php
-define('DB_HOST', 'localhost');
+define('DB_HOST', '10.100.74.226');
 define('DB_NAME', 'pix_dobrado');
-define('DB_USER', 'root2');
+define('DB_USER', 'root');
 define('DB_PASS', '2ktmf4ksed'); // Replace with your actual DB password
 ?>
