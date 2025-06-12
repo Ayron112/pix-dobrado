beba4f3c800820cd0d7f6debaac1e68e762e41d1
Ayron112 <146877747+Ayron112@users.noreply.github.com>
2025-06-12 01:00:39 -0400

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'pix_dobrado');
 define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_PASS', 'P6kZhL7q0z');
 define('DB_PORT', 3306);
 ?>
